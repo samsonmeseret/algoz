ce9c83237ab62cc9e801e3b248c674bbae739621
samsonmeseret <samsonmeseretb@gmail.com>
2023-02-18 16:39:37 -0500

    Admin user previlages update

--- route/userRouter.js
+++ route/userRouter.js
@@ -6,25 +6,30 @@ const photoUploadingFunctions = require("../middlewares/userImageHanddler");
 
 //REGULAR User Activity
 //protecting the activity
+
 Router.use(AuthController.protect);
+
 //1) updating his profiles!
 Router.route("/user/updateMe").patch(
   photoUploadingFunctions.profilePhotoUploader,
   photoUploadingFunctions.resizeUserPhotoUpdate,
   userController.updateMe
 );
+
 //2) Deleting himself
 Router.route("/user/deleteMe").delete(userController.deleteMe);
+
 //3) THE ME ROUTE
 Router.route("/me").get(userController.getMe);
-Router.route("/my/settings").get(userController.getMySetting);
+// Router.route("/my/settings").get(userController.getMySetting);
 
 //ONLY FOR ADMIN
 Router.use(AuthController.restrictTo("admin"));
-Router.route("/manage/Users").get(userController.findAlluser);
+Router.route("/manage/Users").get(userController.findAllUser);
 Router.route("/manage/Users/:id")
   .get(userController.getUser)
-  .patch(userController.updateUserRole)
+  .patch(userController.updateUser)
   .delete(userController.deleteUser);
+
 //exporting it
 module.exports = Router;
